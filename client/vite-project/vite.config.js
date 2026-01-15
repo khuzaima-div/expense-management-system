1e20e846bcad21070c856577206e51131f908c47
khuzaima-div <thekhuzaima0@gmail.com>
2026-01-15 17:44:51 +0500

    fix vite + tailwind + netlify deploy settings

--- client/vite-project/vite.config.js
+++ client/vite-project/vite.config.js
@@ -2,13 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
-
-// https://vite.dev/config/
 export default defineConfig({
-          base:'./',
-
+  base: './', // ✅ important for Netlify
   plugins: [
+    react(),
     tailwindcss(),
-
   ],
 })
