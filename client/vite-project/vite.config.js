46be23d1ebe103d84e58d40b7ae8b219905d9e42
khuzaima-div <thekhuzaima0@gmail.com>
2026-01-15 18:29:57 +0500

    again update

--- client/vite-project/vite.config.js
+++ client/vite-project/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: './', // ✅ Netlify
+  base: '/', // ✅ Isko single slash '/' hona chahiye
   plugins: [
     react(),
     tailwindcss(),
