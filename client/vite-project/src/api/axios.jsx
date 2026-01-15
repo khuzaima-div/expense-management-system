21c5c5937b001a1b8e3960816b4a2a71e12f1243
khuzaima-div <thekhuzaima0@gmail.com>
2026-01-15 19:49:02 +0500

    url update

--- client/vite-project/src/api/axios.jsx
+++ client/vite-project/src/api/axios.jsx
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-baseURL: "https://expense-management-system-rho-nine.vercel.app/"
-
+baseURL: "https://expense-management-system-rho-nine.vercel.app/api"
 });
 
 // Token add
