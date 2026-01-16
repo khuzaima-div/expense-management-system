01b6a243d78c6718a3d4e201881a64e1ef55428d
khuzaima-div <thekhuzaima0@gmail.com>
2026-01-16 18:49:52 +0500

    url fix

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.get("/",(req,res)=>{
 const allowedOrigins = [
   "https://expense-management-system-project.vercel.app", 
   "https://expense-management-system-mocha.vercel.app",
-  "http://localhost:5173" // Local testing ke liye lazmi hai
+  "http://localhost:5173" 
 ];
 
 app.use(cors({
