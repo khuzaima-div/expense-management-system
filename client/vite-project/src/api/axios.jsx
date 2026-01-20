2748319e0125726d802891cdc12221388aaa6b8e
khuzaima-div <thekhuzaima0@gmail.com>
2026-01-20 17:09:47 +0500

    agin msla

--- client/vite-project/src/api/axios.jsx
+++ client/vite-project/src/api/axios.jsx
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-baseURL: "https://expense-management-system-pkrp.vercel.app/api",
-baseURL:"http://localhost:8000/api"
+baseURL: "https://expense-management-system-pkrp.vercel.app/api"
 });
 
 // Token add
