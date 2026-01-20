848fa63d24b15104f22be35fc77e82783c604f14
khuzaima-div <thekhuzaima0@gmail.com>
2026-01-20 17:03:56 +0500

    fournted hai

--- client/vite-project/src/api/axios.jsx
+++ client/vite-project/src/api/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-baseURL: "https://expense-management-system-rho-nine.vercel.app/api",
+baseURL: "https://expense-management-system-pkrp.vercel.app/api",
 baseURL:"http://localhost:8000/api"
 });
 
